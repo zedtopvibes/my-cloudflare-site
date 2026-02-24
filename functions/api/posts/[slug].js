3a6418d77398c8d04a27e2ce97482a04231fa9df
zedtopvibes <zedtopvibes@gmail.com>
2026-02-24 16:11:31 +0200

    Update [slug].js

--- functions/api/posts/[slug].js
+++ functions/api/posts/[slug].js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
             },
-        });
+        }); 
     }
     
     // GET - Fetch single post
