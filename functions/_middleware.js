ea2f0f5cbbdf43d2619f1771225cf57cf6d17c92
zedtopvibes <zedtopvibes@gmail.com>
2026-04-12 20:37:32 +0200

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,20 +1,22 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const path = url.pathname.replace(/^\//, '');
   
-  // List of 14 paths to redirect
+  // Don't redirect admin paths
+  if (url.pathname.startsWith('/admin')) {
+    return context.next();
+  }
+  
+  // List of paths to redirect
   const redirectPaths = [
-    'playlists', 'albums', 'artists', 'tracks', 
-    'genres', 'charts', 'radio', 'podcasts', 
-    'favorites', 'history', 'following', 'liked', 
-    'recent', 'stats', 'settings'
+    '/playlists', '/albums', '/artists', '/tracks', 
+    '/genres', '/charts', '/radio', '/podcasts', 
+    '/favorites', '/history', '/following', '/liked', 
+    '/recent', '/stats', '/settings'
   ];
   
-  // If the path matches, redirect to homepage
-  if (redirectPaths.includes(path)) {
+  if (redirectPaths.includes(url.pathname)) {
     return Response.redirect('/', 301);
   }
   
-  // Otherwise, proceed normally (your admin Functions still work)
   return context.next();
 }
