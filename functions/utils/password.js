8e0743124d465b248fe53c068544fb7f3fe8ca72
zedtopvibes <zedtopvibes@gmail.com>
2026-04-16 16:26:00 +0200

    Create functions/utils/password.js

---
+++ functions/utils/password.js
@@ -0,0 +1,19 @@
+export async function hashPassword(password, salt) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password + salt);
+  const hash = await crypto.subtle.digest('SHA-256', data);
+  return Array.from(new Uint8Array(hash))
+    .map(b => b.toString(16).padStart(2, '0'))
+    .join('');
+}
+
+export function generateSalt() {
+  const array = new Uint8Array(32);
+  crypto.getRandomValues(array);
+  return Array.from(array).map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+export async function verifyPassword(password, hash, salt) {
+  const newHash = await hashPassword(password, salt);
+  return newHash === hash;
+}
