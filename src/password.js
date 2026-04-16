6f4130dcf473db068e3b06fd0c042b1c075f0b20
zedtopvibes <zedtopvibes@gmail.com>
2026-04-16 11:41:36 +0200

    Create src/password.js

---
+++ src/password.js
@@ -0,0 +1,11 @@
+export async function hashPassword(password, salt) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password + salt);
+  const hash = await crypto.subtle.digest('SHA-256', data);
+  return Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+export async function verifyPassword(password, hash, salt) {
+  const newHash = await hashPassword(password, salt);
+  return newHash === hash;
+}
