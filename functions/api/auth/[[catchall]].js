b40963fa66f4425136d3f9543f41feb08d08960f
zedtopvibes <zedtopvibes@gmail.com>
2026-04-16 16:22:20 +0200

    Create functions/api/auth/[[catchall]].js

---
+++ functions/api/auth/[[catchall]].js
@@ -0,0 +1,11 @@
+export async function onRequest() {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+  };
+  
+  return new Response(JSON.stringify({ error: 'Auth endpoint not found' }), {
+    status: 404,
+    headers
+  });
+}
